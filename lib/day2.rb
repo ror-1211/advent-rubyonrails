36da4f23aa0cd2cc84b793f223761875112398e8
ror-1211 <timothyhughest185@gmail.com>
2021-12-02 15:20:31 -0500

    Day 2, part 2

--- lib/day2.rb
+++ lib/day2.rb
@@ -9,22 +9,47 @@ module Day2
     horizontal = 0
     depth = 0
 
+    parse(input_file) { |command, value|
+      case command
+      when "forward" then horizontal += value
+      when "up" then depth -= value
+      when "down" then depth += value
+      else
+        fail "unknown line: #{line}"
+      end
+    }
+
+    horizontal * depth
+  end
+
+  def self.part2(input_file)
+    horizontal = 0
+    depth = 0
+    aim = 0
+
+    parse(input_file) { |command, value|
+      case command
+      when "forward" then
+        horizontal += value
+        depth += (aim * value)
+      when "up" then aim -= value
+      when "down" then aim += value
+      else
+        fail "unknown line: #{line}"
+      end
+    }
+
+    horizontal * depth
+  end
+
+  def self.parse(input_file)
     File
       .read(input_file)
       .each_line
       .map { |line|
         captures = line.match(REGEX).named_captures
-        value = captures["value"].to_i
-        case captures["command"]
-        when "forward" then horizontal += value
-        when "up" then depth -= value
-        when "down" then depth += value
-        else
-          fail "unknown line: #{line}"
-        end
+        yield(captures["command"], captures["value"].to_i)
       }
-
-    horizontal * depth
   end
 
 end
--- spec/day2_spec.rb
+++ spec/day2_spec.rb
@@ -8,4 +8,8 @@ RSpec.describe Day2 do
     expect(Day2.part1("spec/fixtures/day2.txt")).to eq 150
   end
 
+  example "part2" do
+    expect(Day2.part2("spec/fixtures/day2.txt")).to eq 900
+  end
+
 end
